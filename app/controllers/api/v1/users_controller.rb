abe94d420be63165e633501e32d6ec70a8aeebaa
fmoraless <franticzero@gmail.com>
2022-06-08 00:23:15 -0400

    Add products relationship to user#show

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,8 @@ before_action :check_owner, only: %i[update destroy]
 
     # GET /users/1
     def show
-        render json: UserSerializer.new(@user).serializable_hash
+        options = { include: [:products] }
+        render json: UserSerializer.new(@user, options).serializable_hash
     end
 
     def create
