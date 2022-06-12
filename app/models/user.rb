4b9e313f433012c02c5a1c1334df10d0e5d45a4f
fmoraless <franticzero@gmail.com>
2022-06-12 12:06:12 -0400

    Generate orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
-    
+
+    has_many :products, dependent: :destroy
+    has_many :orders, dependent: :destroy
+
     validates :email, uniqueness: true
     validates_format_of :email, with: /@/
     validates :password_digest, presence: true
 
-    has_many :products, dependent: :destroy
+
+
 end
